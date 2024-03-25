ed84333a2600ab6dfd8db39987370f8740a8f399
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-25 14:46:42 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+  export default {
+    components:{
+      Header,
+      Main
+    }
+  }
+</script>
+
+<template>
+  <Header />
+  <Main />
+</template>
+
+
+<style lang="scss" scoped>
+@use './assets/scss/main.scss';
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script>
+import {store} from '../data/store'
+  export default {
+    data(){
+      return{
+        store
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
